72bd113aff8fbd52104fd0d3f4a0551ae8a9ef4d
Jose Ignacio <ji.barraquer@gmail.com>
2018-08-29 11:19:17 -0500

    [add] Home Component

--- src/App.js
+++ src/App.js
@@ -1,42 +1,15 @@
 import React, { Component } from 'react';
 import {
-  StyleSheet,
-  Text,
   View
 } from 'react-native';
+import Home from './screens/Home';
 
 export default class App extends Component {
   render() {
     return (
-      <View style={styles.container}>
-        <Text style={styles.welcome}>
-          Welcome to Civics App!
-        </Text>
-        <Text style={styles.instructions}>
-          To get started, edit App.js
-        </Text>
-        <Text style={styles.instructions}>
-        </Text>
+      <View>
+        <Home />
       </View>
     );
   }
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    justifyContent: 'center',
-    alignItems: 'center',
-    backgroundColor: '#F5FCFF',
-  },
-  welcome: {
-    fontSize: 20,
-    textAlign: 'center',
-    margin: 10,
-  },
-  instructions: {
-    textAlign: 'center',
-    color: '#333333',
-    marginBottom: 5,
-  },
-});
