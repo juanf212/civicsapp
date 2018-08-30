150301c02b3c621f8bea0ac89d03db2d1a62300f
Juan Forero <juanf212@gmail.com>
2018-08-30 10:59:14 -0500

    [add] state, functions, buttons to visualize the screens

--- src/App.js
+++ src/App.js
@@ -1,15 +1,55 @@
 import React, { Component } from 'react';
 import {
-  View
+  View,
+  Button
 } from 'react-native';
 import Home from './screens/Home';
+import SearchResult from './screens/SearchResult';
+import Initiative from './screens/Initiative';
+import Favorite from './screens/Favorite';
+import Login from './screens/Login';
 
 export default class App extends Component {
+
+  state= {
+    screen: <Home />
+  }
+
+  loadHomeScreen = () => {
+    this.setState({ screen:true })
+  }
+
+  loadSearchResultScreen = () => {
+
+    this.setState({screen: <SearchResult /> })
+
+  }
+
+  loadInitiativeScreen = () => {
+    this.setState({ screen: <Initiative /> })
+  }
+  
+  loadLoginScreen = () => {
+    this.setState({ screen: <Login /> })
+  }
+
+  loadFavoriteScreen = () => {
+    this.setState({ screen: <Favorite /> })
+  }
+
   render() {
     return (
+
       <View style={ {flex:1} }>
-        <Home />
+        <Button title="Home" />
+        <Button title="SearchResult" />
+        <Button title="Initiative" />
+        <Button title="Favorite" />
+        <Button title="Login" />
+        {this.state.screen}
       </View>
+
+
     );
   }
 }
