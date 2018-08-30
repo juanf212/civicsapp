4fb8d3636c7da56330f3ed5c702d2644e5640abf
Juan Forero <juanf212@gmail.com>
2018-08-30 10:08:40 -0500

    [add] screens import-export structures

---
+++ src/screens/Initiative.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react';
+import {
+  StyleSheet,
+  Text,
+  View
+} from 'react-native';
+
+export default class Initiative extends Component {
+
+    render() {
+        
+    }
+
+} 
---
+++ src/screens/SearchResult.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react';
+import {
+  StyleSheet,
+  Text,
+  View
+} from 'react-native';
+
+export default class SearchResults extends Component {
+
+    render() {
+        
+    }
+
+} 
