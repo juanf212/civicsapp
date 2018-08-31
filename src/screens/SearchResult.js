5a16ad39bc113e448f04377a84a141b824992111
Jesus Estaba <jeec.estaba@gmail.com>
2018-08-31 11:35:42 -0500

    [refactor] Nearby projects

--- src/screens/SearchResult.js
+++ src/screens/SearchResult.js
@@ -16,75 +16,28 @@ export default class SearchResults extends Component {
     projects: []
   }
 
-  rad = x => x * Math.PI / 180;
-  
-  calculate = (p1, p2) => {
-    let R = 6378137; // Earth’s mean radius in meter
-    let dLat = this.rad(p2.latitude - p1.latitude);
-    let dLong = this.rad(p2.longitude - p1.longitude);
-    let a = Math.sin(dLat / 2) * Math.sin(dLat / 2) +
-      Math.cos(this.rad(p1.latitude)) * Math.cos(this.rad(p2.latitude)) *
-      Math.sin(dLong / 2) * Math.sin(dLong / 2);
-    let c = 2 * Math.atan2(Math.sqrt(a), Math.sqrt(1 - a));
-    let d = R * c;
-    return d / 1000; // returns the distance in kilometers
-  }
-
-
   componentDidMount = () => {
-    CivicsService.cities(cities => {
-      CivicsService.iniciatives(iniciatives => {
-        iniciativesWithCountries = iniciatives.map( iniciative => {
-          iniciative.city = cities[iniciative.city]
-          return iniciative;
-        })
-
-        let projectsInColombia = iniciativesWithCountries.filter( project => project.city.country=='Colombia' )
-        
-
-        let geolocation = new GeolocationService()
-        geolocation.getCoords((diviceCoords)=>{
-          alert('calculando distancias')
-          console.log(diviceCoords)
-          
-          let projectsWithDistace = projectsInColombia.map( (project) => {
-            let {coordinates} = JSON.parse(project.position)
-            let [longitude, latitude] = coordinates;
-            project.distance = this.calculate(diviceCoords, { latitude, longitude})
-            
-            return project
-          })
-
-
-          let nearByProjects = projectsWithDistace
-            .filter( project => project.distance < 5 )
-            .sort( (a,b) => a.distance - b.distance )
-
-          this.setState({
-            projects:nearByProjects
-          })
-
-        })
-
-      })
-    })
+    const geolocation = new GeolocationService()
     
+    geolocation.getCoords((diviceCoords) => {
+    	const civic = new CivicsService()
 
+    	civic.getNearbyProjects( diviceCoords, 5, (projects) => {
+        	this.setState({ projects })
+    	})
+    })
   }
-    render() {
-      return (
-        <View style={styles.container}>
-          <Text style={styles.welcome}>
-            Resultados de Búsqueda
-          </Text>
-          <FlatList 
-            data = { this.state.projects }
-            renderItem = { ({item}) => <SimpleInitiativeLayout {...item}/> }
-          />
+  render() {
+	return (
+    	<View style={styles.container}>
+			<Text style={styles.welcome}>Resultados de Búsqueda</Text>
+			<FlatList 
+				data = { this.state.projects }
+				renderItem = { ({item}) => <SimpleInitiativeLayout {...item}/> }
+			/>
         </View>
-      );      
-    }
-
+    );      
+  }
 } 
 
 const styles = StyleSheet.create({
--- src/services/CivicsService.js
+++ src/services/CivicsService.js
@@ -1,23 +1,66 @@
-const CivicsService = {
-    iniciatives: (callback) => {
+export default class CivicsService {
+    getNearbyProjects = (deviceCoords, range, callback) => {
+        this.iniciativesWithCities(iniciatives => {
+            let projectsWithDistance = iniciatives.map((project) => {
+                let { coordinates } = JSON.parse(project.position)
+                let [longitude, latitude] = coordinates;
+
+                project.distance = this.calculate(deviceCoords, { latitude, longitude })
+
+                return project
+            })
+
+            let nearbyProjects = projectsWithDistance
+                .filter(project => project.distance < range)
+                .sort((a, b) => a.distance - b.distance)
+
+            callback(nearbyProjects)
+        })
+    }
+
+    iniciativesWithCities = (callback) => {
+        this.cities(cities => {
+            this.iniciatives(iniciatives => {
+                let iniciativesWithCities = iniciatives.map(iniciative => {
+                    iniciative.city = cities[iniciative.city]
+                    return iniciative;
+                })
+
+                callback(iniciativesWithCities)
+            })
+        })
+    }
+
+    iniciatives = (callback) => {
         let url = `http://civics.cc/api/initiatives`
         fetch(url)
-            .then( request => request.json() )
+            .then(request => request.json())
             .then(data => {
                 let proyectos = JSON.parse(data)
                 callback(proyectos)
             })
-    },
+    }
 
-    cities: (callback) => {
+    cities = (callback) => {
         let url = `https://civics.cc/api/cities_with_initiatives`
         fetch(url)
-            .then( request => request.json() )
+            .then(request => request.json())
             .then(data => {
                 callback(data)
             })
     }
 
-}
+    rad = x => x * Math.PI / 180;
 
-export default CivicsService;
+    calculate = (p1, p2) => {
+        let R = 6378137; // Earth’s mean radius in meter
+        let dLat = this.rad(p2.latitude - p1.latitude);
+        let dLong = this.rad(p2.longitude - p1.longitude);
+        let a = Math.sin(dLat / 2) * Math.sin(dLat / 2) +
+            Math.cos(this.rad(p1.latitude)) * Math.cos(this.rad(p2.latitude)) *
+            Math.sin(dLong / 2) * Math.sin(dLong / 2);
+        let c = 2 * Math.atan2(Math.sqrt(a), Math.sqrt(1 - a));
+        let d = R * c;
+        return d / 1000; // returns the distance in kilometers
+    }
+}
