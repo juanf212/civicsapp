798e41186928133becc851438d1598bc903738a6
Jesus Estaba <jeec.estaba@gmail.com>
2018-09-04 10:45:56 -0500

    [feature] Favorites delete button

--- src/screens/Favorite.js
+++ src/screens/Favorite.js
@@ -5,6 +5,7 @@ import {
   View,
   AsyncStorage,
   FlatList,
+  Button,
 } from 'react-native';
 
 import SimpleInitiativeLayout from '../components/initiatives/SimpleInitiativeLayout';
@@ -16,11 +17,32 @@ export default class Favorite extends Component {
 
   componentDidMount = async () => {
     let favorites = await AsyncStorage.getItem('favorites');
-    favorites = JSON.parse(favorites)
-  
+	favorites = JSON.parse(favorites)
+	
     this.setState({ favorites })
   }
 
+  	delete = async (id) => {
+		let favorites = this.state.favorites.filter( ({pk}) => pk!=id )
+		
+		await AsyncStorage.setItem('favorites', JSON.stringify(favorites))
+
+		this.setState({ favorites })
+	}
+
+  favoriteLayout = ({item}) => {
+    return (
+    	<View>
+        	<View>
+        		<SimpleInitiativeLayout {...item} initiativeScreen={this.props.initiativeScreen}/>
+        	</View>
+        	<View>
+				<Button color="#ff0000" onPress={ ()=>{ this.delete(item.pk) } } title="Eliminar" />
+        	</View>
+    	</View>
+    )
+  }
+  
   render() {
     return (
       <View style={styles.container}>
@@ -29,8 +51,8 @@ export default class Favorite extends Component {
       </Text>
       <FlatList 
         data = { this.state.favorites }
-        extraData = { (data, index) => index.toString() }
-        renderItem = { ({item}) => <SimpleInitiativeLayout {...item} initiativeScreen={this.props.initiativeScreen}/> }
+        keyExtractor = { (data, index) => index.toString() }
+        renderItem = { this.favoriteLayout }
       />
       </View>
     );      
@@ -42,8 +64,6 @@ export default class Favorite extends Component {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    justifyContent: 'center',
-    alignItems: 'center',
     backgroundColor: '#F5FCFF',
   },
   welcome: {
